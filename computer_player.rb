08da1f8b320fe844ea10c416b13ca2628b5ec096
patricksandquist <patricksandquist@gmail.com>
2016-01-14 12:38:45 -0800

    Add board printout and basic AI

--- computer_player.rb
+++ computer_player.rb
@@ -1,7 +1,15 @@
 require_relative "player.rb"
 
 class ComputerPlayer < Player
-  def get_move
-    
+  def get_move(board, mark)
+    blanks = []
+
+    board.each_with_index do |row, i|
+      row.each_with_index do |el, j|
+        blanks << [i, j] if el == :b
+      end
+    end
+
+    blanks.sample
   end
 end
--- human_player.rb
+++ human_player.rb
@@ -1,8 +1,34 @@
 require_relative "player.rb"
 
 class HumanPlayer < Player
-  def get_move
+  def get_move(board, mark)
+    print_board(board)
     puts "Where would you like to move next?"
     gets.chomp.split(",")
+  end
+
+  private
+
+  def print_board(board)
+    system("clear")
+
+    # Print out the 2D array of the board
+    board.each do |row|
+      output = ""
+
+      row.each do |el|
+        case el
+        when :x
+          output += "X"
+        when :o
+          output += "0"
+        when :b
+          output += " "
+        end
+        output += "|"
+      end
+
+      puts output[0...-1]
+    end
   end
 end
